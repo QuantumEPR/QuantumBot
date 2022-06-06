5e2376e605b5a8be9d633990ec9bb844e2d16c42
QuantumEPR <stevenzheng000131@gmail.com>
2022-06-06 02:23:08 -0700

    fix queue

--- slash/queue.js
+++ slash/queue.js
@@ -12,20 +12,22 @@ module.exports = {
             return await interaction.editReply("There are no songs in the queue")
         }
         const totalPages = Math.ceil(queue.tracks.length / 10) || 1
-        const page = (interaction.options.getNumber("page") || 1)
+        const page = (interaction.options.getNumber("page") || 1) - 1
 
-        if (page > totalPages)
+        if (page + 1 > totalPages)
             return await interaction.editReply(`Invalid Page. There are only a total of ${totalPages} pages of songs`)
         
         const queueString = queue.tracks.slice(page * 10, page * 10 + 10).map((song, i) => {
-            return `**${page * 10 + i + 1}. \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`
-        })
+            return `**${page * 10 + i + 1}.** \`[${song.duration}]\` ${song.title} -- <@${song.requestedBy.id}>`
+        }).join("\n")
 
         const currentSong = queue.current
         await interaction.editReply({
             embeds: [
                 new MessageEmbed()
-                    .setDescription(`**Currently Playing**\n` + (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : "None" ) + `\n\n**Queue**\n${queueString}`)
+                    .setDescription(`
+                        **Currently Playing**\n` +
+                        (currentSong ? `\`[${currentSong.duration}]\` ${currentSong.title} -- <@${currentSong.requestedBy.id}>` : "None" ) + `\n\n**Queue**\n${queueString}`)
                     .setFooter({
                         text: `Page ${page + 1} of ${totalPages}`
                     })
