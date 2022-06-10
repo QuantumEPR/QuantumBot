97a0bbb62c84aad7098a6568397002ddfe27ed01
QuantumEPR <stevenzheng000131@gmail.com>
2022-06-09 22:10:32 -0700

    fix sus image

--- slash/play.js
+++ slash/play.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
+const google = require('googlethis');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -98,12 +99,14 @@ module.exports = {
                 .setThumbnail(song.thumbnail)
                 .setFooter({ text: `Duration: ${song.duration}` })
         } else if (interaction.options.getSubcommand() === "sus") {
-            const susList = [
-                "https://c.tenor.com/1bHFBLnruUUAAAAC/among-us.gif",
-                "https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png",
-                "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F002%2F111%2F316%2Fc57.gif"
-            ]
-            client.user.setAvatar(susList[0])
+            // const susList = [
+            //     "https://c.tenor.com/1bHFBLnruUUAAAAC/among-us.gif",
+            //     "https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png",
+            //     "https://wompampsupport.azureedge.net/fetchimage?siteId=7575&v=2&jpgQuality=100&width=700&url=https%3A%2F%2Fi.kym-cdn.com%2Fphotos%2Fimages%2Fnewsfeed%2F002%2F111%2F316%2Fc57.gif"
+            // ]
+            const images = await google.image('Amogus', { safe: false });
+            const image = images[Math.floor(Math.random() * images.length)]
+            client.user.setAvatar(image)
             let url = "https://www.youtube.com/watch?v=ekL881PJMjI"
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
@@ -113,7 +116,7 @@ module.exports = {
             await queue.addTrack(song)
             embed
                 .setDescription("S U S")
-                .setImage(client.user.displayAvatarURL({dynamic : true}))
+                .setImage(image)
             await queue.play()
             await interaction.editReply({
                 embeds: [embed]
